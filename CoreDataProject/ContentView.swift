1ecc858ad004101c4eafcb9933e6147d448b9a4c
Krzysztof Grech <krzysztofgrech.poz@gmail.com>
2022-08-17 09:21:36 +0200

    Basics completed

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    let students: [Student] = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
+    @State var counter = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List(students, id: \.self) {student in
+                Text(student.name)
+            }
+            
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown Wizard")
+            }
+            
+            Button("Add Wizard") {
+                let wizard = Wizard(context: moc)
+                wizard.name = "\(counter) Harry"
+                counter += 1
+            }
+            
+            Button("Save") {
+                if moc.hasChanges {
+                    do {
+                        try moc.save()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Krzysztof Grech on 17/08/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
