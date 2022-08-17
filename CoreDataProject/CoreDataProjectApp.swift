cf0c431b083a9e0c84e9db34ec0ff413b28e72bb
Krzysztof Grech <krzysztofgrech.poz@gmail.com>
2022-08-17 07:44:56 +0200

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Krzysztof Grech on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
